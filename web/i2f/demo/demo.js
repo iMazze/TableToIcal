605d9c1108219a7d670d14be9203f2cda3d7ef3b
Maximilian Klug <imazze@android-365479e38897c50e.fritz.box>
2019-01-03 20:30:01 +0100

    Added some Webthings

--- web/i2f/demo/demo.js
+++ web/i2f/demo/demo.js
@@ -1,7 +1,7 @@
 ics_sources = [
     {url:'samples/events.ics',event_properties:{color:'gold'}},
     {url:'samples/events-noend.ics',event_properties:{color:'pink'}},
-    {url:'samples/32c3.ics'},
+    {url:'https://imazze.github.io/TableToIcal/bin/TFE17-2%20-%204.%20Semester.ics'},
     {url:'samples/daily_recur.ics',event_properties:{className:['daily-recur'], url:'http://recurring.events.example.org/'}}
 ]
 
