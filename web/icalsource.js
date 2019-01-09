666cb92193a778435543a0c934e42706594db4c4
Maximilian Klug <imazze@Maximilians-MacBook-Pro.local>
2019-01-09 15:18:07 +0100

    ade some important changes

--- web/icalsource.js
+++ web/icalsource.js
@@ -1,7 +1,3 @@
-/*global ICAL,$,log,moment */
-// Any copyright is dedicated to the Public Domain.
-// http://creativecommons.org/publicdomain/zero/1.0/
-
 // Return a Promise that resolves to jCal data of the ICAL file at `url`.
 function get_ical_data(url) {
   return new Promise((resolve, reject) => {
@@ -37,7 +33,7 @@ function ical_event_source(url) {
             .map(entry => {
               return {
                 //id: entry.uid,
-                title: entry.summary,
+                title: 'xxx',//entry.summary,
                 allDay: entry.startDate.isDate,
                 start: entry.startDate.toJSDate(),
                 end: entry.endDate.toJSDate(),
@@ -49,4 +45,58 @@ function ical_event_source(url) {
       });
     }
   };
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function ical_event_source2(url) {	
+	var events = []
+	var parsed = ICAL.parse(httpGet(url));
+	parsed[2].forEach(function(event) {
+	  if (event[0] !== 'vevent') return
+	  var summary, location, start, end;
+	  event[1].forEach(function(event_item){
+		
+		switch(event_item[0]){
+		  case 'location':
+			location = event_item[3]
+			break;
+		  case 'summary':
+			summary = event_item[3]
+			break;
+		  case 'dtstart':
+			start = event_item[3]
+			break;
+		  case 'dtend':
+			end = event_item[3]
+			break;
+		}
+		if (summary && location && start && end) {
+		  events.push({
+			title: summary,
+			start: start,
+			end: end,
+			location: location
+		  })  
+		}
+	  })
+	});
+	
+	// What the Hell it works
+	var events_small = [];
+    var i;
+    for (i = 0; i < events.length; i++) { 
+    	if(i%2==0)
+    	{
+    		events_small.push(events[i]);
+    	}
+	}
+       
+    return events_small;
 }
